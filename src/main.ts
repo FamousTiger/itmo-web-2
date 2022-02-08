8ad10871598e99ebc007fe16dfcf784f4cdbe211
Nikolay Fedotenko <kalkolay@protonmail.com>
2022-02-08 19:35:01 +0300

    Add frontend from the previous semester

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   const port = new ConfigService().get<number>('PORT') || 12345;
   await app.listen(port);
 }
